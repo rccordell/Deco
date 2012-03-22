82a03e08cac8a00c7eb0f79727301af6b0560ed4
Matt Scott <mscott62@students.kennesaw.edu>
2012-03-22 20:34:19 -0300

    Improves compatibility with servers running default php settings as <? php short tags are disabled by default.

--- deco/items/show.php
+++ deco/items/show.php
@@ -3,7 +3,7 @@
 <h1><?php echo item('Dublin Core', 'Title'); ?></h1>
 
 <!-- start FancyBox initialization and configuration -->
-<? echo js('fancybox/fancybox-init-config');?>		
+<?php echo js('fancybox/fancybox-init-config');?>		
 
 <!-- Video JS init -->
 <script type="text/javascript">VideoJS.setupAllWhenReady();</script>
@@ -144,7 +144,7 @@
 	
 
 <!-- the edit button for logged in superusers and admins -->
-<? $current = Omeka_Context::getInstance()->getCurrentUser();
+<?php $current = Omeka_Context::getInstance()->getCurrentUser();
         if ($current->role == 'super') {
                 echo '<p><a class="edit-button" href="'. html_escape(uri('admin/items/edit/')).item('ID').'">Edit this item...</a></p>';
                 }
